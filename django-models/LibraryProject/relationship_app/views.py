af22bf5beba6f3fdaa664c9eade2722f030d2b5b
Dominic Solomon Agiode <d.mcdad623@gmail.com>
2025-11-09 21:26:57 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -93,15 +93,16 @@ def _is_member(user):
 
 @user_passes_test(_is_admin, login_url='login')
 def admin_view(request):
-    return render(request, 'myapp/admin_view.html')
+    return render(request, 'relationship_app/admin_view.html')
 
 @user_passes_test(_is_librarian, login_url='login')
 def librarian_view(request):
-    return render(request, 'myapp/librarian_view.html')
+    return render(request, 'relationship_app/librarian_view.html')
 
 @user_passes_test(_is_member, login_url='login')
 def member_view(request):
-    return render(request, 'myapp/member_view.html')
+    return render(request, 'relationship_app/member_view.html')
+
 
 
 
