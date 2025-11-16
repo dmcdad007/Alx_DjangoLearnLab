1dab855ae6f0946105547c3b83f9d7598561809b
Dominic Solomon Agiode <d.mcdad623@gmail.com>
2025-11-16 09:56:27 +0100

    Update admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -12,3 +12,21 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ("title", "author")
     ordering = ("-publication_year", "title")
     list_per_page = 25
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff', 'is_superuser')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
