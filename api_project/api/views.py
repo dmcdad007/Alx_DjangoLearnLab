e18756e5c96f5a8fec098dddf0b9b69c146c14a6
Dominic Solomon Agiode <d.mcdad623@gmail.com>
2025-11-23 08:42:14 +0100

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -7,3 +7,18 @@ class BookList(ListAPIView):
     serializer_class = BookSerializer
 
 generics.ListAPIView
+
+from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
+from .models import Book
+from .serializers import BookSerializer
+
+# Existing ListAPIView
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# New ViewSet for full CRUD
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
