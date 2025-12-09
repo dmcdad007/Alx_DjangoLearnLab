75ba6d15001b85e01cf8486000f5c5dba82e7b2f
Dominic Solomon Agiode <d.mcdad623@gmail.com>
2025-12-09 16:30:43 +0100

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -12,3 +12,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
