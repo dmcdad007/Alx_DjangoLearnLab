2025666592650b329bef62abe08f99bbc09b33de
Dominic Solomon Agiode <d.mcdad623@gmail.com>
2025-12-13 11:51:09 +0100

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('register/', register_view, name='register'),
     path('profile/', profile_view, name='profile'),
 ]
+
 from django.urls import path
 from .views import (
     PostListView, PostDetailView, PostCreateView,
